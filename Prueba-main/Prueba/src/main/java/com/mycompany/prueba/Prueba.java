1f483e17720618e9f2c86e4fa86f8c3029f5c055
MichaelNivar <khabibmike16@gmail.com>
2023-01-18 22:13:15 +0100

    OTRO

---
+++ Prueba-main/Prueba/src/main/java/com/mycompany/prueba/Prueba.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.prueba;
+
+/**
+ *
+ * @author micro
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Nueva Rama");
+        System.out.println("HOLAAAAAAAAAAAA");
+        System.out.println("MICHAEL");
+    }
+
+}
